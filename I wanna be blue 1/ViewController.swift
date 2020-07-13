4ac08ecc3abfcc06f1c9854617636623a34199f0
Megan Simmons <m24simmons@gmail.com>
2020-07-13 12:57:04 -0500

    Initial Commit

---
+++ I wanna be blue 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I wanna be blue 1
+//
+//  Created by Student on 7/13/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
